d556e65e832dccc9e02fc6ba6e5720aa33b34731
KenzaVianda <82711690+KenzaVianda@users.noreply.github.com>
2021-04-18 00:59:30 +0700

    Update Modul4_Kel27.cpp

--- Modul4_Kel27.cpp
+++ Modul4_Kel27.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+using namespace std;
+void X(){
+	string judul = "##### Luas Persegi #####";
+	int sisi,LP;
+	cout<<judul<<endl;
+	cout<<"masukan nilai sisi =";cin>>sisi;
+		LP=sisi*sisi;
+		cout<<"\nLuas persegi ="<<LP;
+	}
 string Y(){
 	string judul2 ="##### Luas persegi panjang #####";
 	int panjang,lebar,LPP;
