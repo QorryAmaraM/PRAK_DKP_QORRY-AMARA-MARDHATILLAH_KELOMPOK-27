b42caa383c27bf049e03722641a7b112f89d0fdf
QorryAmaraM <82710479+QorryAmaraM@users.noreply.github.com>
2021-04-18 00:40:45 +0700

    Add files via upload

---
+++ Modul4_Kel27.cpp
@@ -0,0 +1,9 @@
+string Y(){
+	string judul2 ="##### Luas persegi panjang #####";
+	int panjang,lebar,LPP;
+	cout<<"masukan nilai lebar =";cin>>lebar;
+	cout<<"masukan nilai panjang =";cin>>panjang;
+		LPP=panjang*lebar;
+		cout<<"\nLuas persegi panjang ="<<LPP;
+	}
+
