ed5ca7c0ac1b44a1f640d0e650a3e8bcd5fc10d3
Zef Lois <82568198+Zeflois@users.noreply.github.com>
2021-04-18 11:34:29 +0700

    Update Modul4_Kel27.cpp

--- Modul4_Kel27.cpp
+++ Modul4_Kel27.cpp
@@ -26,3 +26,17 @@ Int main(){
 		cout<<"1.Luas persegi\n";
 		cout<<"2.Luas persegi panjang\n";
 		cout<<"pilih(1,2) =";cin>>pilih;
+switch(pilih){
+			case '1' :
+				X();
+				break;
+			case '2' :
+				Y();
+				break;
+			default :
+				cout<< "Maaf angka yang anda pilih tidak tersedia dalam program ini\n;";
+			}
+		cout<<"\n\nIngin Mengulang Perhitungan ?(y/n)=";cin>>ulang;
+	} 
+	while(ulang=='y'||ulang=='Y');
+}
